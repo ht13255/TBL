c29b5a49dd9352382199391e94d10dfe462a699e
ssad <88150876+ht13255@users.noreply.github.com>
2026-07-16 08:05:49 +0900

    Update TBL to version 1.1.0

--- src/openphotontwin/__init__.py
+++ src/openphotontwin/__init__.py
@@ -4,19 +4,28 @@ The package combines exact small-Fock-space linear optics with a time-resolved
 Monte Carlo hardware digital twin for time-bin and fiber-loop experiments.
 """
 
-from .adapters import from_perceval, from_sax_model, from_sparameters, from_strawberry_fields
+from .adapters import (
+    from_perceval,
+    from_sax_model,
+    from_sparameter_sweep,
+    from_sparameters,
+    from_strawberry_fields,
+)
 from .backends import ArrayBackend, get_backend, propagate_batch
 from .calibration import (
     CalibrationResult,
     CoincidenceHistogram,
+    HOMBootstrap,
     HOMFit,
     LossEstimate,
     LossProfile,
     ModelComparison,
     auto_calibrate,
+    bootstrap_hom_fit,
     calibration_report,
     coincidence_histogram,
     compare_to_ideal,
+    estimate_accidental_coincidences,
     estimate_loss,
     fit_hom_dip,
     load_time_tags,
@@ -28,6 +37,7 @@ from .circuit import (
     FockSimulator,
     LinearOpticalCircuit,
     MatrixComponent,
+    SpectralMatrixComponent,
     permanent,
 )
 from .components import (
@@ -47,16 +57,27 @@ from .components import (
 from .detectors import SNSPD, DetectorArray, TimeTag
 from .errors import OpenPhotonTwinError, OptionalDependencyError, SimulationError, ValidationError
 from .experiments import HOMResult, hom_probabilities, hom_scan
-from .models import Photon, PhotonEvent, SinglePhotonSource, TimeBinQubit, Wavepacket
+from .models import (
+    CorrelatedPhotonSource,
+    Photon,
+    PhotonEvent,
+    SinglePhotonSource,
+    TimeBinQubit,
+    Wavepacket,
+)
 from .simulator import DigitalTwin, SimulationResult
+from .timebin import CoherentLoopResult, CoherentTimeBinLoop
 
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 
 __all__ = [
     "ArrayBackend",
     "BeamSplitter",
     "CalibrationResult",
     "CoincidenceHistogram",
+    "CoherentTimeBinLoop",
+    "CoherentLoopResult",
+    "CorrelatedPhotonSource",
     "DelayLine",
     "DetectorArray",
     "DigitalTwin",
@@ -66,6 +87,7 @@ __all__ = [
     "FiberLoop",
     "FockDistribution",
     "FockSimulator",
+    "HOMBootstrap",
     "HOMFit",
     "HOMResult",
     "LinearOpticalCircuit",
@@ -85,19 +107,23 @@ __all__ = [
     "TemperaturePhaseDrift",
     "SimulationError",
     "SimulationResult",
+    "SpectralMatrixComponent",
     "SinglePhotonSource",
     "TimeBinQubit",
     "TimeTag",
     "ValidationError",
     "Wavepacket",
     "auto_calibrate",
+    "bootstrap_hom_fit",
     "calibration_report",
     "coincidence_histogram",
     "compare_to_ideal",
     "estimate_loss",
+    "estimate_accidental_coincidences",
     "fit_hom_dip",
     "from_perceval",
     "from_sax_model",
+    "from_sparameter_sweep",
     "from_sparameters",
     "from_strawberry_fields",
     "get_backend",
